c3c374b658199223fd502fd86757f180bba410b9
Shawn Yang <shawny56@hotmail.com>
2018-08-21 00:27:01 +0000

    new docker groovy 002

--- job-dsl/nodejsdocker.groovy
+++ job-dsl/nodejsdocker.groovy
@@ -14,7 +14,7 @@ job('NodeJS Docker example') {
     }
     steps {
         dockerBuildAndPublish {
-            repositoryName('shawnyang56/docker-nodejs-001')
+            repositoryName('shawny56/jenkins-001')
             tag('${GIT_REVISION,length=9}')
             registryCredentials('dockerhub')
             forcePull(false)
